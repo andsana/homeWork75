82ec828bd29459e84fff999c0dc58b7916e8a681
Susanna Andreeva <sanadex90@gmail.com>
2024-01-19 18:16:34 +0900

    Add interaction with the Vigenère cipher API in the page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
-export default function Home() {
+'use client';
+import VigenereForm from '@/frontend / VigenereForm/ VigenereForm';
+import {Vigenere} from "@/types";
+import {useState} from "react";
+import axios from "axios";
 
-  return (
-    <main>
-      Hello, world!
-    </main>
-  );
+export default function Home() {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmit = async (vigenere: Vigenere, action: 'encode' | 'decode') => {
+        setIsLoading(true);
+        try {
+            const response = await axios.post('/', vigenere);
+
+            if (action === 'encode') {
+                console.log('Encoded:', response.data.encoded);
+            } else {
+                console.log('Decoded:', response.data.decoded);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <VigenereForm onSubmit={onSubmit} isLoading={isLoading}/>
+    );
 }
